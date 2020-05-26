785dbf1d3918cc68794cf38cd058a02707afb029
Olly Campbell <olly@ollycampbell.co.uk>
2020-05-26 14:55:15 +0100

    wip

--- pages/index.tsx
+++ pages/index.tsx
@@ -15,22 +15,22 @@ const HELLO_QUERY = gql`
 `
 
 const Home = () => {
-  // const [result, reexecuteQuery] = useQuery({
-  //   query: HELLO_QUERY,
-  //   variables: { world: 'mars' },
-  // })
-  // const { data, fetching, error } = result
-  // if (fetching) return <p>Loading...</p>
-  // if (error) return <p>Oh no... {error.message}</p>
-  // console.log(data)
+  const [result, reexecuteQuery] = useQuery({
+    query: HELLO_QUERY,
+    variables: { world: 'mars' },
+  })
+  const { data, fetching, error } = result
+  if (fetching) return <p>Loading...</p>
+  if (error) return <p>Oh no... {error.message}</p>
+  console.log(data)
 
   return (
     <div>
       <p>{`${publicRuntimeConfig.BASE_URL}/api/graphql`}</p>
-      {/* <p>{data?.hello?.name}</p>
+      <p>{data?.hello?.name}</p>
       <button onClick={() => reexecuteQuery({ requestPolicy: 'network-only' })}>
         Refetch()
-      </button> */}
+      </button>
     </div>
   )
 }
